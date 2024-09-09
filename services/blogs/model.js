d18016b33cf7bb6e697291fe12b72c688d6f2810
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2024-09-09 20:32:34 +0200

    Add files via upload

---
+++ services/blogs/model.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose"
+import Comment from "../comments/model.js"
+const blogsSchema = new Schema(
+  {
+    category: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    readTime: {
+      value: {
+        type: Number,
+        required: true
+      },
+      unit: {
+        type: String,
+        required: true
+      },
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "Author",
+    },
+    comments: [{
+      "text": String,
+      "author": {
+        type: Schema.Types.ObjectId,
+        ref: "Author",
+      },
+  }]
+  },
+  { collection: "blogs" }
+)
+
+export default model("Blogs", blogsSchema)
